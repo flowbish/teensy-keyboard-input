badd5196a14bc9eee2c4ce8be0af66df1f14fbac
Porter Smith <flowbish@gmail.com>
2015-10-10 17:04:20 -0500

    simple 10-key keyboard functionality

---
+++ src/keydefs.h
@@ -0,0 +1,13 @@
+/**
+ * teensy-keyboard-input
+ * Copyright (c) 2015 Porter Smith
+ */
+
+#ifndef KEYDEFS_H__
+#define KEYDEFS_H__
+
+#define KEY(k) KEY_##k
+#define KEY_PRESSED 0
+#define KEY_RELEASED 1
+
+#endif 
--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,65 @@
 #define USB_KEYBOARD
 #include "usb_dev.h"
 #include "usb_keyboard.h"
+#include "keydefs.h"
+
+
+#define USB_IDLE_RATE 500
+
+/**
+ * Use SPI with the following pins
+ * SS = PIN_10
+ * SCK = PIN_13
+ * MOSI = PIN_11
+ * MISO = PIN_12
+ */
+
+#define NUM_KEYS 10
+int key_inputs[NUM_KEYS] = {KEY_RELEASED};
+int key_keycodes[NUM_KEYS] = {KEY(A),
+                              KEY(S),
+                              KEY(E),
+                              KEY(T),
+                              KEY(SPACE),
+                              KEY(ENTER),
+                              KEY(N),
+                              KEY(I),
+                              KEY(O),
+                              KEY(P)};
+int key_pins[NUM_KEYS] = {14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
 
 extern "C" int main(void) {
+    int i;
 
-    uint8_t chars[] = "abcdefghijklmnopqrstuvwxyz1234567890-="
-                      "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+";
+    // Set each pin for our keys as an input and enable the pullup resistor on each
+    //  pin. This is necessary since we connect our switches to ground.
+    for (i = 0; i < NUM_KEYS; i++) {
+        pinMode(key_pins[i], INPUT_PULLUP);
+    }
     
     usb_init();
 	while (1) {
-        size_t c = random() % sizeof(chars);
-        usb_keyboard_write(chars[c]);
-        delay(30);
+        int current_inputs[NUM_KEYS] = {KEY_PRESSED};
+
+        // Read the status of each switch connected (digital 0 means pressed).
+        for (i = 0; i < NUM_KEYS; i++) {
+            current_inputs[i] = digitalRead(key_pins[i]);
+
+            // test for falling edge (press)
+            // TODO: debounce
+            if (current_inputs[i] == KEY_PRESSED && key_inputs[i] == KEY_RELEASED) {
+                usb_keyboard_press_keycode(key_keycodes[i]);
+            }
+
+            // test for rising edge (release)
+            // TODO: debounce
+            else if (current_inputs[i] == KEY_RELEASED && key_inputs[i] == KEY_PRESSED) {
+                usb_keyboard_release_keycode(key_keycodes[i]);
+            }
+
+            key_inputs[i] = current_inputs[i];
+        }
+        delayMicroseconds(3000);
 	}
 }
 
